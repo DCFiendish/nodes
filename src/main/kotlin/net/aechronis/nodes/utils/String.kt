43882f1c40a19d8806216b9af66e6d1a32319c91
luna <lunap@tuta.io>
2026-07-13 23:05:17 +0100

    refactor: streamline JSON collection formatting

--- src/main/kotlin/net/aechronis/nodes/utils/String.kt
+++ src/main/kotlin/net/aechronis/nodes/utils/String.kt
@@ -2,34 +2,12 @@
  * Utils for parsing collections to strings
  *
  * Used by serializer
- * TODO: benchmark if string array funcs are necessary
  */
 
 package net.aechronis.nodes.utils
 
-inline fun <T> stringArrayFromSet(iter: Set<T>, itemName: (T) -> String): String {
-    var s = "["
-    for ((i, v) in iter.withIndex()) {
-        s += itemName(v)
-        if (i < iter.size - 1) {
-            s += ","
-        }
-    }
-    s += "]"
+fun <T> stringArrayFromSet(iter: Set<T>, itemName: (T) -> String): String = iter.joinToString(separator = ",", prefix = "[", postfix = "]", transform = itemName)
 
-    return s
-}
-
-inline fun <K, V> stringMapFromMap(iter: Map<K, V>, keyString: (K) -> String, valString: (V) -> String): String {
-    var s = "{"
-    for ((i, entry) in iter.entries.withIndex()) {
-        val key = keyString(entry.key)
-        val value = valString(entry.value)
-        s += "$key:$value"
-        if (i < iter.size - 1) {
-            s += ","
-        }
-    }
-    s += "}"
-    return s
+fun <K, V> stringMapFromMap(iter: Map<K, V>, keyString: (K) -> String, valString: (V) -> String): String = iter.entries.joinToString(separator = ",", prefix = "{", postfix = "}") { (key, value) ->
+    "${keyString(key)}:${valString(value)}"
 }
