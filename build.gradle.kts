13e68714d9cda953c30e10434269235e6aee68d4
DC Fiendish <johnpatricl.ortman@gmail.com>
2026-08-04 20:58:11 -0400

    chore: bump utils pin to 86a747b to match combat/vanilla (was on stale eff1c8c)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 
 dependencies {
     implementation("net.minestom:minestom:2026.07.12-26.2")
-    implementation("net.aechronis:utils:eff1c8c")
+    implementation("net.aechronis:utils:86a747b")
 
     // testing
     testImplementation("org.jetbrains.kotlin:kotlin-test")
